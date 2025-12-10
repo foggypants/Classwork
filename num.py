638323e003a35d36fa1142e508a9a1bdc83675d0
foggypants <watsupd2@gmail.com>
2025-12-10 11:35:18 +0530

    message

--- num.py
+++ num.py
@@ -1,9 +1,17 @@
 import numpy as np  
-
+'''
 arr=np.array(['a','2','3'], dtype='a')
 print(arr)
 print(arr.dtype)
-'''
+
 arr=np.array(["apple","banana","cherry"])
 print(arr.dtype)
 '''
+arr=np.array([1.1,2.2,3.3])
+newarr=arr.astype('i')
+print(newarr)
+print(newarr.dtype)
+arr=np.array([1,0,3])
+newarr=arr.astype('f')
+print(newarr)
+print(newarr.dtype)
