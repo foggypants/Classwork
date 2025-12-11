9451ba6dff19a33b3154956ac28c16ee72d20ba1
foggypants <watsupd2@gmail.com>
2025-12-11 11:55:36 +0530

    Update

--- num.py
+++ num.py
@@ -6,12 +6,44 @@ print(arr.dtype)
 
 arr=np.array(["apple","banana","cherry"])
 print(arr.dtype)
-'''
-arr=np.array([1.1,2.2,3.3])
-newarr=arr.astype('i')
+
+arr=np.array([0.10,0.20,0.13])
+print(arr)
+print(arr.dtype)
+newarr=arr.astype(bool)
 print(newarr)
 print(newarr.dtype)
 arr=np.array([1,0,3])
 newarr=arr.astype('f')
 print(newarr)
 print(newarr.dtype)
+
+arr=np.array([1,2,3,4,5])
+x=arr.copy()
+arr[0]=42
+print(arr)
+print(x)
+x=arr.view()
+arr[1]=99
+print(arr)
+print(x)
+
+arr=np.array([1,2,3,4,5])
+x=arr.view()
+x[0]=31
+print(arr)
+print(x)
+arr=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+print(arr.reshape(6,2))
+
+def f(x,y=5,*args):
+    return x+y+sum(args)
+print(f(2,3,4,5))
+'''
+a=np.arange(0,9).reshape(3,3)
+print(a[1,:])
+x=None
+if x:
+    print("yes")
+else:
+    print("no")
